7b0eaf32a2e9b03a71e23fa2449bfef57410ade2
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-14 21:42:48 +0530

    implemented a basic template of react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NavBar from "./NavBar"
 
 function App() {
 
   return (
     <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/login" element={<div>Login Page</div>} />
+          <Route path="/test" element={<div>Test Page</div>} />
+        </Routes>
+
+
+      </BrowserRouter>
       <NavBar />
     </>
   )
