e91ed4a94eccd81ae1d9ef7cb59cce93de69c544
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-16 14:40:18 +0530

    created a feed component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./Login"
 import Profile from "./Profile"
 import { Provider } from "react-redux"
 import { appStore } from "./utils/appStore"
+import Feed from "./Feed"
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
             </Route>
