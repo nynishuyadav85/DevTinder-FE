e05a61624d16348464f5d514057ef8ba31c11e45
Nishant Yadav <nynishuyadav85@gmail.com>
2025-05-02 20:26:14 +0530

    called a BE /feed api for all the users in a DB

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice'
 
 const Feed = () => {
+    const feed = useSelector((store) => store.feed)
+    const dispatch = useDispatch()
+
+    const getFeed = async () => {
+        if (feed) return;
+        try {
+            const res = await axios.get(`${BASE_URL}/feed`, { withCredentials: true })
+            dispatch(addFeed(res.data))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getFeed();
+    }, [])
     return (
         <div>Feed</div>
     )
