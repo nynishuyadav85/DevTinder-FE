d5bdf059e576566bb02e066d27a91ad6562ea0c3
Nishant Yadav <nynishuyadav85@gmail.com>
2025-05-12 23:33:02 +0530

    add Connections component with data fetching functionality

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios'
 
 const Connections = () => {
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/users/connections`, { withCredentials: true });
+      console.log(res.data.data[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchConnections()
+  }, [])
+
+
   return (
     <div>Connections</div>
   )
