23f1ca7722486829cc8298b0c7885312892702f3
Nishant Yadav <nynishuyadav85@gmail.com>
2025-05-14 01:53:25 +0530

    add connectionSlice and integrate with Connections component for data fetching

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants'
 import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { addConnections } from '../utils/connectionSlice'
 
 const Connections = () => {
+  const connections = useSelector((store) => store.connections)
+  const dispatch = useDispatch()
 
   const fetchConnections = async () => {
     try {
       const res = await axios.get(`${BASE_URL}/users/connections`, { withCredentials: true });
-      console.log(res.data.data[0])
+      console.log(res.data.data)
+      dispatch(addConnections(res.data.data))
     } catch (error) {
       console.log(error)
     }
