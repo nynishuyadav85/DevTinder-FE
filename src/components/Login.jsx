1b3452baaecaddf867edb676f7331ba00227d1dd
Nishant Yadav <nynishuyadav85@gmail.com>
2025-05-27 18:04:44 +0530

    feat: update import statement in Login component to improve readability

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import { useNavigate } from 'react-router-dom';
