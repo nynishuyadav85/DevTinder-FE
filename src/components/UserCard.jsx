3c553cac14dd91c2a4a8bf00aa4d3c9d68a095a3
Nishant Yadav <nynishuyadav85@gmail.com>
2025-05-27 21:12:26 +0530

    feat: implement handleSendRequest function in UserCard and update feedSlice to remove user from feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { removeUserFromFeed } from '../utils/feedSlice'
 
 const UserCard = ({ user }) => {
-    const { firstName, lastName, age, photoUrl, about, gender } = user || {}
-    console.log(user)
-    const handleRequest = () => {
-        console.log("Todo")
+    const dispatch = useDispatch()
+    const { _id, firstName, lastName, age, photoUrl, about, gender } = user || {}
+    const handleSendRequest = async (status, userId) => {
+        try {
+            const res = await axios.post(`${BASE_URL}/request/send/` + status + "/" + userId, {}, { withCredentials: true })
+            dispatch(removeUserFromFeed(userId))
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }
     }
     return (
         <div><div className="card bg-base-100 w-96 shadow-sm">
@@ -18,8 +27,8 @@ const UserCard = ({ user }) => {
                 {age && gender && <p>{age + " " + gender}</p>}
                 <p>{about}</p>
                 <div className="card-actions justify-center my-4">
-                    <button className="btn btn-primary" onClick={handleRequest}>Ignore</button>
-                    <button className="btn btn-secondary">Send Request</button>
+                    <button className="btn btn-primary" onClick={() => handleSendRequest("ignored", _id)}>Ignore</button>
+                    <button className="btn btn-secondary" onClick={() => handleSendRequest("intrested", _id)}>Send Request</button>
                 </div>
             </div>
         </div></div>
